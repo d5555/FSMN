f5c7f938dbe4c003980523b2d0590cdcb52b987d
d5555 <gitprojects5@gmail.com>
2022-07-15 15:27:38 -0500

    Update FSMN.py

--- FSMN.py
+++ FSMN.py
@@ -1,9 +1,39 @@
-class scFSMNCell(nn.Module): #compact scalar FSMN with bidirectional option
+
+class sFSMNCell(nn.Module): #scalar FSMN
+    def __init__(self, memory_size, input_size, output_size, bidirectional=False, drop=0.1, device=None, dtype=torch.float32):
+        super().__init__()
+        factory_kwargs={'device':device, 'dtype':dtype}
+        self.device = device
+        #memory_size: 0 - no memory, 1 - one memory step, 2 - ...
+        self._memory_size = memory_size = memory_size + 1
+        self.dropout = nn.Dropout(p=drop)
+        self.bidirectional=bidirectional
+
+        self._W1 = nn.Parameter(torch.randn(input_size, output_size))
+        self._W2 = nn.Parameter(torch.randn(input_size, output_size))
+        self._bias = nn.Parameter(torch.randn( output_size))  
+
+        nn.init.xavier_uniform_(self._W1)
+        nn.init.xavier_uniform_(self._W2)
+
+    def forward(self, input_data, pad_mask=None):
+        num_steps = input_data.size(1) 
+
+        memory=torch.ones((num_steps,num_steps), requires_grad=False).tril(-1).cumsum(0).triu(- self._memory_size+1)
+        if self.bidirectional: memory = memory + memory.t()
+        memory = memory.unsqueeze(0).expand(input_data.size(0),-1,-1).to(self.device)
+        if pad_mask is not None: memory=pad_mask.unsqueeze(1)*memory
+        h_hatt = torch.matmul(memory, input_data)
+        h = torch.matmul(input_data, self._W1 )
+        h += torch.matmul(h_hatt, self._W2) + self._bias
+        return h
+    
+class csFSMNCell(nn.Module): # compact scalar FSMN with bidirectional option
     def __init__(self, memory_size, input_size, output_size, bidirectional=False , drop=0.1, device=None, dtype=torch.float32):
         super().__init__()
         factory_kwargs={'device':device, 'dtype':dtype}
         self.device = device
-        #memory_size: 0 - no memory, 1 - step, 2...
+        #memory_size: 0 - no memory, 1 - one memory step, 2...
         self._memory_size = memory_size = memory_size + 1
         self._dtype = dtype
         self.bidirectional=bidirectional
@@ -14,11 +44,6 @@ class scFSMNCell(nn.Module): #compact scalar FSMN with bidirectional option
         self._W2 = nn.Parameter(torch.randn(output_size, output_size))
         self._bias1 = nn.Parameter(torch.randn( output_size))  
         self._bias2 = nn.Parameter(torch.randn( output_size))
-        #self.linear1 = nn.Linear(input_size, input_size*2)
-        #self.linear2 = nn.Linear(input_size*2, input_size)
-
-        #self.embeddings_table = nn.Parameter(torch.Tensor(max_relative_position * 2 + 1, num_units).to(device))
-        #nn.init.xavier_uniform_(self.embeddings_table)
 
         nn.init.xavier_uniform_(self._W1)
         nn.init.xavier_uniform_(self._W2)
@@ -27,8 +52,6 @@ class scFSMNCell(nn.Module): #compact scalar FSMN with bidirectional option
         num_steps = input_data.size(1) 
 
         p = torch.matmul(input_data, self._W1 ) + self._bias1
-        #print(torch.ones((num_steps,num_steps)).tril(-1).cumsum(0).triu(- self._memory_size+1).fill_diagonal_(1))
-        #memory0=  torch.ones((input_data.size(0),num_steps,num_steps)).tril(-1).cumsum(1).triu(- self._memory_size+1).to(self.device)
         memory=torch.ones((num_steps,num_steps), requires_grad=False).tril(-1).cumsum(0).triu(- self._memory_size+1)
         if self.bidirectional: memory = memory + memory.t()
         memory = memory.fill_diagonal_(1).unsqueeze(0).expand(input_data.size(0),-1,-1).to(self.device)
@@ -36,3 +59,110 @@ class scFSMNCell(nn.Module): #compact scalar FSMN with bidirectional option
         p = torch.matmul(memory, p)
         p = torch.matmul(p, self._W2 ) + self._bias2
         return p
+    
+class vFSMNCell(nn.Module): #vectorized FSMN
+    def __init__(self, memory_size, input_size, output_size, bidirectional=False, drop=0.1, device=None, dtype=torch.float32):
+        super().__init__()
+        factory_kwargs={'device':device, 'dtype':dtype}
+        self.device = device
+        #memory_size: 0 - no memory, 1 - step, 2...
+        self._memory_size = memory_size = memory_size + 1
+        #self._output_size = output_size
+        #self._input_size = input_size
+        self._dtype = dtype
+        #self.activation=activation
+        self.bidirectional=bidirectional
+
+        #self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=drop)
+
+        self._W1 = nn.Parameter(torch.randn(input_size, output_size))
+        self._W2 = nn.Parameter(torch.randn(input_size, output_size))
+        self._bias = nn.Parameter(torch.randn( output_size))  
+        #self.linear1 = nn.Linear(input_size, input_size*2)
+        #self.linear2 = nn.Linear(input_size*2, input_size)
+
+        #self.embeddings_table = nn.Parameter(torch.Tensor(max_relative_position * 2 + 1, num_units).to(device))
+        embed_tensor = torch.Tensor(memory_size, input_size).to(device)
+        self.embeddings_table = nn.Parameter(embed_tensor)
+        nn.init.xavier_uniform_(self.embeddings_table)
+        with torch.no_grad(): self.embeddings_table[0]=0 # not nessesary 
+
+        nn.init.xavier_uniform_(self._W1)
+        nn.init.xavier_uniform_(self._W2)
+
+    def forward(self, input_data, pad_mask=None):
+        num_steps = input_data.size(1) 
+    
+        memory=torch.ones((num_steps,num_steps), requires_grad=False).tril(-1).cumsum(0).triu(- self._memory_size+1).long()
+        if self.bidirectional: memory = memory + memory.t()
+        memory = memory.unsqueeze(0).expand(input_data.size(0),-1,-1).to(self.device)
+        if pad_mask is not None: memory=pad_mask.unsqueeze(1)*memory
+        with torch.no_grad(): self.embeddings_table[0]=0
+        memory = self.embeddings_table[memory].to(self.device)
+
+        h_hatt = torch.einsum('bijd,bjd->bid', memory, input_data)#'bijd,bjd->bid'
+        h = torch.matmul(input_data, self._W1 )
+        h += torch.matmul(h_hatt, self._W2) + self._bias
+        return h
+        
+class cvFSMNCell(nn.Module): #compact vectorized FSMN
+    def __init__(self, memory_size, input_size, output_size, bidirectional=False, drop=0.1, device=None, dtype=torch.float32):
+        super().__init__()
+        factory_kwargs={'device':device, 'dtype':dtype}
+        self.device = device
+        #memory_size: 0 - no memory, 1 - step, 2...
+        self._memory_size = memory_size = memory_size + 1
+
+        self._dtype = dtype
+        self.bidirectional=bidirectional
+        self.dropout = nn.Dropout(p=drop)
+
+        self._W1 = nn.Parameter(torch.randn(input_size, output_size))
+        self._W2 = nn.Parameter(torch.randn(output_size, output_size))
+        self._bias1 = nn.Parameter(torch.randn( output_size))  
+        self._bias2 = nn.Parameter(torch.randn( output_size))
+
+        embed_tensor=torch.Tensor(memory_size, output_size) 
+        self.embeddings_table = nn.Parameter(embed_tensor)
+        nn.init.xavier_uniform_(self.embeddings_table)
+        with torch.no_grad(): self.embeddings_table[0]=0
+
+        nn.init.xavier_uniform_(self._W1)
+        nn.init.xavier_uniform_(self._W2)
+
+    def forward(self, input_data, pad_mask=None):
+        num_steps = input_data.size(1) 
+        p = torch.matmul(input_data, self._W1 ) + self._bias1
+
+        memory=torch.ones((num_steps,num_steps), requires_grad=False).tril(-1).cumsum(0).triu(- self._memory_size+1).long()
+        if self.bidirectional: memory = memory + memory.t()
+        memory = memory.fill_diagonal_(1).unsqueeze(0).expand(input_data.size(0),-1,-1).to(self.device)
+        if pad_mask is not None: memory=pad_mask.unsqueeze(1)*memory
+        with torch.no_grad(): self.embeddings_table[0]=0
+        memory = self.embeddings_table[memory].to(self.device)
+        
+        p = torch.einsum('bijd,bjd->bid', memory, p)#'bijd,bjd->bid'
+        p = torch.matmul(p, self._W2 ) + self._bias2
+        return p
+    
+class FSMN(nn.Module): # FSMN layer
+    def __init__(self, memory_size, input_size, hidden_size, output_size, n_layers, fsmncell, d_ff, drop=0.1,activation=F.relu, bidirectional=False, device=None, dtype=torch.float32):
+      super().__init__()
+      factory_kwargs={'device':device, 'dtype':dtype}
+      self.activation = activation#nn.GELU()
+      self.dropout = nn.Dropout(p=drop)
+      self.linear1 = nn.Linear(hidden_size, d_ff)
+      self.linear2 = nn.Linear(d_ff, output_size)
+      self.norm1 = nn.LayerNorm(hidden_size, **factory_kwargs)
+      #self.norm2 = nn.LayerNorm(input_size, **factory_kwargs)
+
+      first_layer = fsmncell(memory_size, input_size, hidden_size, bidirectional, drop, **factory_kwargs)
+      self.fsmn_layers = nn.ModuleList([ first_layer,*[
+          fsmncell(memory_size, hidden_size, hidden_size, bidirectional, drop, **factory_kwargs)
+          for _ in range(n_layers-1)   ]])
+
+    def forward(self, x, pad_mask=None):
+      for layer in self.fsmn_layers: 
+        x = self.activation(self.norm1(layer(x, pad_mask)))
+      return self.linear2(self.dropout(self.activation(self.linear1(x))))  
